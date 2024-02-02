0d1d6e2e7198892627b0c5c06b8747db033c16c7
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-02-02 19:21:30 +0300

    Day1

--- day1/part2/calibration_evaluator2.js
+++ day1/part2/calibration_evaluator2.js
@@ -15,16 +15,37 @@ function processFile(content){
 }
 const digits = {one:'one1one', two:'two2two', three:'three3three', four:'four4four', five:'five5five',six:'six6six', seven:'seven7seven', eight:'eight8eight', nine: 'nine9nine'}
 function numberConverter(content) {
-    let result = '';
+    let sum = 0;
     for (let line of content){
         for( let num of Object.keys(digits)){
             line = line.replaceAll(num,digits[num] );
             
         }
-        result += line + '\n';
-    }
-    console.log(result)
-}
-function extractValues(content){
+        let firstValue= (line) =>{
+            const match = line.match(/\d/);
+            if (match){
+                return parseInt(match[0],10)
+            }
+            return null;
+        }
+        let lastValue=(line) =>{
+            let originalArray = Array.from(String(line), Number)
+            let reverseArray = originalArray.slice().reverse();
+            let reverseString = reverseArray.join('');
+            const match = reverseString.match(/\d/);
+            if (match){
+                return parseInt(match[0],10);
+            }
+            return null;
+        }
+        
+        console.log(line)
+        let calibrationValue= parseInt(firstValue(line) + '' +lastValue(line)+'');
+        console.log(calibrationValue)
+        sum += calibrationValue;
+        console.log(sum)
+        
 
-} 
+    }
+}
+
